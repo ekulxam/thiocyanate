885ea3dcc00fe6911a03af17ec247589b370a9da
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-01-26 22:25:17 -0800

    Target the real one

--- src/main/java/survivalblock/thiocyanate/cyanide/mixin/accessor/ResourceManagerRegistryLoadTaskAccessor.java
+++ src/main/java/survivalblock/thiocyanate/cyanide/mixin/accessor/ResourceManagerRegistryLoadTaskAccessor.java
@@ -2,7 +2,7 @@
 package survivalblock.thiocyanate.cyanide.mixin.accessor;
 
 import net.minecraft.core.RegistrationInfo;
-import net.minecraft.resources.RegistryDataLoader;
+import net.minecraft.resources.ResourceManagerRegistryLoadTask;
 import net.minecraft.server.packs.repository.KnownPack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
@@ -11,7 +11,7 @@ import java.util.Optional;
 import java.util.function.Function;
 
 @SuppressWarnings({"UnusedMixin", "unused"})
-@Mixin(RegistryDataLoader.class)
+@Mixin(ResourceManagerRegistryLoadTask.class)
 public interface ResourceManagerRegistryLoadTaskAccessor {
     @Accessor("REGISTRATION_INFO_CACHE")
     static Function<Optional<KnownPack>, RegistrationInfo> thiocyanate$getRegistrationInfoCache() {
